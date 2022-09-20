54a6706ff1401891b0e906a66d87a68222aeaa67
paubc15 <55392836+paubc15@users.noreply.github.com>
2022-09-20 11:55:29 +0200

    Update Optimitzador.py

--- Optimitzador.py
+++ Optimitzador.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 import csv
 import math
 
-def f (rank, c, b, a):
-    return c / ((rank+b)**a)
+
+def f (rank, a, b, c):
+    return (c / ((rank+b)**a))
 
 line_count = 0
 x = []
@@ -26,17 +27,17 @@ with open('news.csv') as csv_file:
 
 y.reverse()
 
-params, covs = curve_fit(f, x, y) 
-print("params: ", params) 
-c, b, a = params[0], params[1], params[2]
+params, covs = curve_fit(f, x, y, bounds=([0.0, 0.0, 0.0],[2., 100000.0, 1000000.0]))
+print("params: ", params)
+a, b, c = params[0], params[1], params[2]
 yfit = c / ((x+b)**a)
 
-#plt.xscale("log")
-#plt.yscale("log")
+plt.xscale("log")
+plt.yscale("log")
 plt.plot(x, y, 'bo', label="y-original")
 plt.plot(x, yfit, label="yfit")
 plt.xlabel('x')
 plt.ylabel('y')
 plt.legend(loc='best', fancybox=True, shadow=True)
 plt.grid(True)
-plt.show() 
+plt.show()
