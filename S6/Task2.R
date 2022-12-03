7b6e4072d2aa3a5a0acdcb4f168fd7c11f5e1ea7
pau bosch <paubc8@gmail.com>
2022-12-03 08:48:30 +0100

    S6

--- S6/Task2.R
+++ S6/Task2.R
@@ -6,9 +6,15 @@ length(V(g))
 diameter(g)
 transitivity(g)
 degree.distribution(g)
+page_rank <- page_rank(g)$vector
+node_size = page_rank*20
+plot(g, vertex.label=NA, vertex.size=node_size)
 
-1. Describe the network a little. How many edges and nodes does it have? What is its diameter? And transitivity?
-  And degree distribution? Does it look like a random network? Visualize the network with node sizes proportional
-to their pagerank.
+
+wc <- walktrap.community(g)
+modularity(wc)
+membership(wc)
+plot(g, vertex.color=membership(wc))
+
 2. Now, use a community detection algorithm of your choice from the list provided. How many nodes does the
 largest community found contain? Plot the histogram of community sizes. Plot the graph with its communities.
