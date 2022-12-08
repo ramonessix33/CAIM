b4ba0c9e38ddd40ab373fe3cb2d803a002393b5d
pau bosch <paubc8@gmail.com>
2022-12-08 17:36:26 +0100

    S6

--- S6/Task2.R
+++ S6/Task2.R
@@ -16,6 +16,8 @@ modularity(wc)
 membership(wc)
 plot(g, vertex.color=membership(wc))
 
-hist(membership(wc))
+h <- hist(membership(wc), breaks=c(0,1,2,3,4,5,6,7,8))
+text(h$mids,h$counts,labels=h$counts, adj=c(0.5, -0.5))
 
+h
      
